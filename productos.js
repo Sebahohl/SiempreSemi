d675a6135b255d72f3edd3e3c7579ee1eb4a38bc
Seba Hohl <sebahohl06@gmail.com>
2022-10-18 18:54:11 -0300

    Actualizacion

--- productos.js
+++ productos.js
@@ -1,18 +1,18 @@
 const productos = [{
     id: 1,
-    foto: "https://www.innovaventas.com/tienda/wp-content/uploads/2020/07/camara-web-imilab-cmsxj22a-2mp-full-hd-2-300x300.jpg",
+    foto: "https://t2.uc.ltmcdn.com/es/posts/4/5/2/como_cuidar_las_unas_postizas_42254_600_square.jpg",
     nombre: "Uñas kapping",
     precio: 800
 },
 {
     id: 2,
-    foto: "https://cdn.shopify.com/s/files/1/0055/6400/6434/products/51Mov6F8z5L_800x_2cccac65-8649-4cf8-9bb7-eefc1feafc29_300x.jpg?v=1602717301",
+    foto: "https://cdn.shopify.com/s/files/1/0279/5539/6653/products/offerma-esmalte-semipermanente-indigo-2_800x.jpg?v=1608657066",
     nombre: "Uñas esmaltado semipermanente",
     precio: 800
 },
 {
     id: 3,
-    foto: "https://www.elkjop.no/image/dv_web_D180001002180018/10014/chromecast-3-generasjon.jpg?$prod$",
+    foto: "https://i.pinimg.com/736x/2e/00/2e/2e002e8acd6e82a9e046dbf8b5b4f55a.jpg",
     nombre: "Uñas esculpidas",
     precio: 1200
 },
