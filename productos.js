dcf9b652eec15a8394be04f5a3e9695a2295f414
Seba Hohl <sebahohl06@gmail.com>
2022-10-18 17:26:42 -0300

    js

---
+++ productos.js
@@ -0,0 +1,19 @@
+const productos = [{
+    id: 1,
+    foto: "https://www.innovaventas.com/tienda/wp-content/uploads/2020/07/camara-web-imilab-cmsxj22a-2mp-full-hd-2-300x300.jpg",
+    nombre: "Uñas kapping",
+    precio: 800
+},
+{
+    id: 2,
+    foto: "https://cdn.shopify.com/s/files/1/0055/6400/6434/products/51Mov6F8z5L_800x_2cccac65-8649-4cf8-9bb7-eefc1feafc29_300x.jpg?v=1602717301",
+    nombre: "Uñas esmaltado semipermanente",
+    precio: 800
+},
+{
+    id: 3,
+    foto: "https://www.elkjop.no/image/dv_web_D180001002180018/10014/chromecast-3-generasjon.jpg?$prod$",
+    nombre: "Uñas esculpidas",
+    precio: 1200
+},
+];
--- proyecto.js
+++ proyecto.js
@@ -1,4 +1,4 @@
-
+d
 
 
 
@@ -28,8 +28,57 @@
 // }
 
 
-const servicios = ["Uñas Kapping $800","Uñas esculpidas $1200","Uñas esmaltado semipermanente $800"];
+// const servicios = ["Uñas Kapping $800","Uñas esculpidas $1200","Uñas esmaltado semipermanente $800"];
 
-console.log(servicios)
+// console.log(servicios)
 
 
+const carrito = [];
+let totalCarrito;
+let contenedor = document.getElementById("misprods");
+
+function renderizarProds(){
+    for(const producto of productos){
+        contenedor.innerHTML += `
+        <div class="card col-sm-2">
+        <img src= ${producto.foto} class="card-img-top" alt="...">
+            <div class=card-body">
+                <h5 class="card-title">${producto.id}</h5>
+                <p class="card-text">${producto.nombre}</p>
+                <p class="card-text">$ ${producto.precio}</p>
+                <button id="boton" class="btn btn-primary">Comprar</button>
+            </div>
+        </div>
+        `;
+    }
+}
+renderizarProds();
+
+
+//EVENTOS
+    productos.forEach(producto => {
+        //evento para cada boton
+        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
+            agregarAlCarrito(producto);
+        });
+    })
+
+
+renderizarProds();
+
+function agregarAlCarrito(productoComprado){
+    carrito.push(productoComprado);
+    console.table(carrito);
+    alert("Producto: "+productoComprado.nombre+" agregado al carrito!");
+    document.getElementById("tablabody").innerHTML += `
+        <tr>
+            <td>${productoComprado.id}</td>
+            <td>${productoComprado.nombre}</td>
+            <td>${productoComprado.precio}</td>
+        </tr>
+    `;
+    totalCarrito = carrito.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText="Total a pagar $: "+totalCarrito;
+
+}
