ac4757c50afc3902249345a39be806f818098b9d
Seba Hohl <sebahohl06@gmail.com>
2022-10-25 23:02:25 -0300

    proyecto js

--- proyecto.js
+++ proyecto.js
@@ -1,34 +1,4 @@
 
-// let nombre = prompt ("Ingresa tu nombre ")
-// alert("Bienvenida/o a siempre semi "+nombre );
-// alert("Siempre semi es mi emprendimiento, es mi pasión hacer uñas y espero que te gusten mis servicios :D");
-
-// menuOpt = prompt("Desea saber que servicios hay? (si/no)");
-
-// let unias=prompt("Ingresa el tipo de uñas que desea realizarse (Kapping - Esculpidas - Esmaltado semipermanente) (s para salir)");
-
-// while(unias!="s"){
-//     switch (unias){
-//         case "Kapping":
-//             alert("El precio de las uñas kapping es de $800");
-//             break;
-//         case "Esculpidas":
-//             alert("El precio de las uñas esculpidas es de $1200");
-//             break;
-//         case "Esmaltado semipermanente":
-//             alert("El precio de las uñas de esmaltado semipermanente es de $800");
-//             break;
-//     }
-
-//     unias=prompt("Desea ver algun precio más? (s para salir)");
-// }
-
-
-// const servicios = ["Uñas Kapping $800","Uñas esculpidas $1200","Uñas esmaltado semipermanente $800"];
-
-// console.log(servicios)
-
-
 const carrito = [];
 let totalCarrito;
 let contenedor = document.getElementById("misprods");
